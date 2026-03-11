790268f3e367c45310c43fc71dd30e3b76ffc317
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-11 21:53:30 +0100

    Update rpc.ts

--- plugins/src/DiscordRichPresence/src/rpc.ts
+++ plugins/src/DiscordRichPresence/src/rpc.ts
@@ -1,18 +1,5 @@
-// src/rpc.ts
+import { Activity } from "./types";
 
-// Interfejs Activity wyraźnie eksportowany
-export interface Activity {
-    name: string;
-    application_id: string;
-    type: number;
-    details: string;
-    state: string;
-    timestamps: { _enabled: boolean; start: number };
-    assets: { large_image: string; large_text: string; small_image: string; small_text: string };
-    buttons: { label: string; url: string }[];
-}
-
-// Klasa RPCClient eksportowana jako default
 export default class RPCClient {
     clientId: string;
 

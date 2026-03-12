de5b6e372a72bfba18dd190b5207a9f291ce0939
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-12 15:34:47 +0100

    Update types.ts

--- plugins/src/DiscordRichPresence/src/types.ts
+++ plugins/src/DiscordRichPresence/src/types.ts
@@ -1,28 +1,30 @@
-export interface Assets {
-  large_image?: string;
-  large_text?: string;
-  small_image?: string;
-  small_text?: string;
+export interface ActivityAssets {
+  large_image: string;
+  large_text: string;
+  small_image: string;
+  small_text: string;
+}
+
+export interface ActivityMetadata {
+  button_urls: [string, string];
 }
 
 export interface Activity {
   application_id: string;
   name: string;
-  details?: string;
-  state?: string;
-  type?: number;
-  timestamps?: { start?: number; end?: number };
-  assets?: Assets;
-  buttons?: string[];
-  metadata?: { button_urls?: string[] };
+  details: string;
+  state: string;
+  type: number;
+  timestamps?: { start: number };
+  assets: ActivityAssets;
+  buttons: [string, string];
+  metadata: ActivityMetadata;
 }
 
-export interface Settings {
-  application_id: string;
-  name: string;
-  details?: string;
-  state?: string;
-  assets?: Assets;
-  buttons?: string[];
-  metadata?: { button_urls?: string[] };
-}
+// Typy dostępnych aktywności do FormSelect
+export const ActivityTypes = [
+  { label: "Playing", value: 0 },
+  { label: "Streaming", value: 1 },
+  { label: "Listening", value: 2 },
+  { label: "Watching", value: 3 },
+];

e1159b4ba699690bf2e078c41ce8ee9a4426a78e
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-12 15:27:44 +0100

    Update settings.tsx

--- plugins/src/DiscordRichPresence/src/settings.tsx
+++ plugins/src/DiscordRichPresence/src/settings.tsx
@@ -1,25 +1,30 @@
-import { Forms, General } from "@vendetta/ui/components";
+import { React, ReactNative } from "@vendetta/metro/common";
+import { Forms } from "@vendetta/ui/components";
 import { storage } from "@vendetta";
-import type { Settings as S } from "./types";
+
+const { View, ScrollView } = ReactNative;
+const { FormText, FormSection } = Forms;
 
 export default () => {
-  const update = (key: keyof S, value: any) => storage[key] = value;
+  const update = (key: string, value: any) => storage[key] = value;
 
   return (
-    <General.Section title="Discord Rich Presence Settings">
-      <Forms.Text label="Application ID" defaultValue={storage.application_id || ""} onChange={v => update("application_id", v)} />
-      <Forms.Text label="Name" defaultValue={storage.name || ""} onChange={v => update("name", v)} />
-      <Forms.Text label="Details" defaultValue={storage.details || ""} onChange={v => update("details", v)} />
-      <Forms.Text label="State" defaultValue={storage.state || ""} onChange={v => update("state", v)} />
-      <Forms.Text label="Type" defaultValue={storage.state || ""} onChange={v => update("state", v)} />
-      <Forms.Text label="Large Image Key" defaultValue={storage.assets?.large_image || ""} onChange={v => storage.assets = { ...(storage.assets || {}), large_image: v }} />
-      <Forms.Text label="Large Image Text" defaultValue={storage.assets?.large_text || ""} onChange={v => storage.assets = { ...(storage.assets || {}), large_text: v }} />
-      <Forms.Text label="Small Image Key" defaultValue={storage.assets?.small_image || ""} onChange={v => storage.assets = { ...(storage.assets || {}), small_image: v }} />
-      <Forms.Text label="Small Image Text" defaultValue={storage.assets?.small_text || ""} onChange={v => storage.assets = { ...(storage.assets || {}), small_text: v }} />
-      <Forms.Text label="Button 1 Label" defaultValue={storage.buttons?.[0] || ""} onChange={v => storage.buttons = [v, storage.buttons?.[1] || ""]} />
-      <Forms.Text label="Button 1 URL" defaultValue={storage.metadata?.button_urls?.[0] || ""} onChange={v => storage.metadata = { button_urls: [v, storage.metadata?.button_urls?.[1] || ""] }} />
-      <Forms.Text label="Button 2 Label" defaultValue={storage.buttons?.[1] || ""} onChange={v => storage.buttons = [storage.buttons?.[0] || "", v]} />
-      <Forms.Text label="Button 2 URL" defaultValue={storage.metadata?.button_urls?.[1] || ""} onChange={v => storage.metadata = { button_urls: [storage.metadata?.button_urls?.[0] || "", v] }} />
-    </General.Section>
+    <ScrollView>
+      <FormSection title="Discord Rich Presence Settings">
+        <FormText label="Application ID" defaultValue={storage.application_id || ""} onChangeText={v => update("application_id", v)} />
+        <FormText label="Name" defaultValue={storage.name || ""} onChangeText={v => update("name", v)} />
+        <FormText label="Details" defaultValue={storage.details || ""} onChangeText={v => update("details", v)} />
+        <FormText label="State" defaultValue={storage.state || ""} onChangeText={v => update("state", v)} />
+        <FormText label="type" defaultValue={storage.type || ""} onChangeText={v => update("type", v)} />
+        <FormText label="Large Image Key" defaultValue={storage.assets?.large_image || ""} onChangeText={v => storage.assets = { ...(storage.assets || {}), large_image: v }} />
+        <FormText label="Large Image Text" defaultValue={storage.assets?.large_text || ""} onChangeText={v => storage.assets = { ...(storage.assets || {}), large_text: v }} />
+        <FormText label="Small Image Key" defaultValue={storage.assets?.small_image || ""} onChangeText={v => storage.assets = { ...(storage.assets || {}), small_image: v }} />
+        <FormText label="Small Image Text" defaultValue={storage.assets?.small_text || ""} onChangeText={v => storage.assets = { ...(storage.assets || {}), small_text: v }} />
+        <FormText label="Button 1 Label" defaultValue={storage.buttons?.[0] || ""} onChangeText={v => storage.buttons = [v, storage.buttons?.[1] || ""]} />
+        <FormText label="Button 1 URL" defaultValue={storage.metadata?.button_urls?.[0] || ""} onChangeText={v => storage.metadata = { button_urls: [v, storage.metadata?.button_urls?.[1] || ""] }} />
+        <FormText label="Button 2 Label" defaultValue={storage.buttons?.[1] || ""} onChangeText={v => storage.buttons = [storage.buttons?.[0] || "", v]} />
+        <FormText label="Button 2 URL" defaultValue={storage.metadata?.button_urls?.[1] || ""} onChangeText={v => storage.metadata = { button_urls: [storage.metadata?.button_urls?.[0] || "", v] }} />
+      </FormSection>
+    </ScrollView>
   );
 };

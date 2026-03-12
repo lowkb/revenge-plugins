5f08f454af538482f23f2cf8b80602967c764e97
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-12 18:48:31 +0100

    Update index.ts

--- plugins/src/AlwaysActive/src/index.ts
+++ plugins/src/AlwaysActive/src/index.ts
@@ -1,18 +1,23 @@
 import { FluxDispatcher } from "@vendetta/metro/common";
+import { logger } from "@vendetta";
 
-let interval;
+let interval: NodeJS.Timer;
 
 export default {
   onLoad() {
+    logger.log("AlwaysActive plugin loaded");
+    
     interval = setInterval(() => {
       FluxDispatcher.dispatch({
         type: "IDLE",
         idle: false
       });
+      logger.log("Sent active heartbeat to prevent idle");
     }, 20000);
   },
 
   onUnload() {
     clearInterval(interval);
+    logger.log("AlwaysActive plugin unloaded");
   }
 };

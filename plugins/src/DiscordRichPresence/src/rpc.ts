cf94fd8ccd94aa6cbcc3d6115d5e3b99826e4e90
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-12 16:35:31 +0100

    Update rpc.ts

--- plugins/src/DiscordRichPresence/src/rpc.ts
+++ plugins/src/DiscordRichPresence/src/rpc.ts
@@ -2,5 +2,14 @@ import { FluxDispatcher } from "@vendetta/metro/common";
 import type { Activity } from "./types";
 
 export const sendRPC = (activity: Activity, pid = 1608, socketId = "rpc") => {
-  FluxDispatcher.dispatch({ type: "LOCAL_ACTIVITY_UPDATE", pid, socketId, activity });
+    
+  FluxDispatcher.dispatch(
+    { 
+      type: "LOCAL_ACTIVITY_UPDATE", 
+      pid, 
+      socketId, 
+      activity 
+    }
+  );
+  logger.log("send" + activity);
 };

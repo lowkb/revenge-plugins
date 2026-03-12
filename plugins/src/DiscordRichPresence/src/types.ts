8f8dc5edafcd5f9b88d7234a9a01c24e3b2bf6fc
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-12 14:56:39 +0100

    Update types.ts

--- plugins/src/DiscordRichPresence/src/types.ts
+++ plugins/src/DiscordRichPresence/src/types.ts
@@ -1,10 +1,29 @@
-export interface Activity {
-    name: string;
-    application_id: string;
-    type: number;
-    details: string;
-    state: string;
-    timestamps: { _enabled: boolean; start: number };
-    assets: { large_image: string; large_text: string; small_image: string; small_text: string };
-    buttons: { label: string; url: string }[];
+export interface RPCAssets {
+  large_image?: string;
+  large_text?: string;
+  small_image?: string;
+  small_text?: string;
+}
+
+export interface RPCActivity {
+  application_id: string;
+  name: string;
+  details?: string;
+  state?: string;
+  type?: number;
+  timestamps?: { start?: number; end?: number };
+  assets?: RPCAssets;
+  buttons?: string[];
+  metadata?: { button_urls?: string[] };
+}
+
+export interface RPCSettings {
+  application_id: string;
+  name: string;
+  details?: string;
+  state?: string;
+  type?: number;
+  assets?: RPCAssets;
+  buttons?: string[];
+  metadata?: { button_urls?: string[] };
 }

6ef6593f5fddb9aaa9ed4d52dd7021c204eb3a5a
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-25 22:00:12 +0100

    Update EvalView.tsx

--- plugins/src/BetterDiscordEval/src/EvalView.tsx
+++ plugins/src/BetterDiscordEval/src/EvalView.tsx
@@ -2,10 +2,11 @@ import { React, ReactNative } from "@vendetta/metro/common";
 import { Forms } from "@vendetta/ui/components";
 import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
-
+import { logger } from "@vendetta";
 const { FormInput, FormSection, FormSwitchRow } = Forms;
 
 export default function EvalView() {
+  logger.log("evalview")
   useProxy(storage);
 
   storage.code ??= "";

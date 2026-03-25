0d080bc8423002966a3fceb058670bd33a6a60e5
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-25 21:27:59 +0100

    Update Eval.ts

--- plugins/src/BetterDiscordEval/src/Eval.ts
+++ plugins/src/BetterDiscordEval/src/Eval.ts
@@ -1,1 +1,15 @@
 
+import { storage } from "@vendetta/plugin";
+import { findByProps } from "@vendetta/metro";
+
+const util = findByProps("inspect");
+
+export async function runEval(code: string): Promise<string> {
+  try {
+    const res = eval(`${code}//# sourceURL=BetterEval`);
+    const final = storage.awaitResult ? await res : res;
+    return util.inspect(final, { showHidden: storage.showHidden ?? false });
+  } catch (e) {
+    return e instanceof Error ? e.message : String(e);
+  }
+}

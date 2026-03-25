70f2e5e0a0ff13432635141e06cf534534be1568
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-25 21:44:17 +0100

    Update Eval.ts

--- plugins/src/BetterDiscordEval/src/Eval.ts
+++ plugins/src/BetterDiscordEval/src/Eval.ts
@@ -1,19 +1,15 @@
 import { storage } from "@vendetta/plugin";
 import { logger } from "@vendetta";
 
-export const storageKey = "BetterDiscordEval";
-
-export const storage = {
-  code: ""
-};
-
 export async function runEval() {
   if (!storage.code) return;
 
   try {
     const result = await eval(storage.code);
     logger.log("[Eval Result]", result);
+    return result;
   } catch (err) {
     logger.error("[Eval Error]", err);
+    return err;
   }
 }

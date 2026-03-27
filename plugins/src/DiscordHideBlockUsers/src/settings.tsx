f2f99f480e795871ef89e18d2b4e5650be251da1
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-27 21:27:01 +0100

    Update settings.tsx

--- plugins/src/DiscordHideBlockUsers/src/settings.tsx
+++ plugins/src/DiscordHideBlockUsers/src/settings.tsx
@@ -1,3 +1,4 @@
+
 import { React, ReactNative } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
@@ -8,22 +9,25 @@ const { View, ScrollView } = ReactNative;
 export default function Settings() {
     useProxy(storage);
 
+    storage.blocked ??= true;
+    storage.ignored ??= true;
+
     return (
         <ScrollView>
             <View>
                 <Forms.FormSwitchRow
                     label="Remove blocked messages"
-                    value={storage.blocked ?? true}
+                    value={storage.blocked}
                     onValueChange={(v) => (storage.blocked = v)}
                     note=""
                 />
                 <Forms.FormSwitchRow
                     label="Remove ignored messages"
-                    value={storage.ignored ?? true}
+                    value={storage.ignored}
                     onValueChange={(v) => (storage.ignored = v)}
                     note=""
                 />
             </View>
         </ScrollView>
     );
-                                           }
+}

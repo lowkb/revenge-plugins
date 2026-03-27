21b29a84d7380e4f9ec91618ab201a1851c349fd
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-27 14:08:36 +0100

    Update settings.tsx

--- plugins/src/DiscordHideBlockUsers/src/settings.tsx
+++ plugins/src/DiscordHideBlockUsers/src/settings.tsx
@@ -12,9 +12,27 @@ export default function Settings() {
         <ScrollView>
             <View>
                 <Forms.FormSwitchRow
-                    label="Hide blocked & ignored users"
+                    label="Enable hiding"
                     value={storage.enabled ?? true}
                     onValueChange={(v: boolean) => (storage.enabled = v)}
+                />
+
+                <Forms.FormSwitchRow
+                    label="Hide blocked users"
+                    value={storage.blocked ?? true}
+                    onValueChange={(v: boolean) => (storage.blocked = v)}
+                />
+
+                <Forms.FormSwitchRow
+                    label="Hide ignored users"
+                    value={storage.ignored ?? true}
+                    onValueChange={(v: boolean) => (storage.ignored = v)}
+                />
+
+                <Forms.FormSwitchRow
+                    label="Hide replies to blocked/ignored"
+                    value={storage.removeReplies ?? true}
+                    onValueChange={(v: boolean) => (storage.removeReplies = v)}
                 />
             </View>
         </ScrollView>

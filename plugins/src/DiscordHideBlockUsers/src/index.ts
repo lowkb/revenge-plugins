028be5ecbfed6a71884f04186709992601bcdae1
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-27 16:19:14 +0100

    Update index.ts

--- plugins/src/DiscordHideBlockUsers/src/index.ts
+++ plugins/src/DiscordHideBlockUsers/src/index.ts
@@ -7,7 +7,7 @@ import Settings from "./settings";
 
 const RowManager = findByName("RowManager");
 const { isBlocked, isIgnored } = findByProps("isBlocked", "isIgnored");
-// Check if a user should be filtered
+
 const isFilteredUser = (id?: string) => {
     if (!id) return false;
     if (storage.blocked && isBlocked(id)) return true;
@@ -15,19 +15,16 @@ const isFilteredUser = (id?: string) => {
     return false;
 };
 
-// Check if a message or its reply should be filtered
 const filterMessage = (msg: any) => {
     if (!msg) return false;
-    // Filter message if author is blocked/ignored
     if (isFilteredUser(msg.author?.id)) return true;
-    // Always filter replies to blocked/ignored users
-    if (msg.referenced_message) {
-        if (isFilteredUser(msg.referenced_message.author?.id)) return true;
+    if (msg.referenced_message?.author?.id) {
+        if (isFilteredUser(msg.referenced_message.author.id)) return true;
     }
     return false;
 };
 
-let patches: any[] = [];
+let patches: (() => void)[] = [];
 
 const startPlugin = () => {
     try {
@@ -35,46 +32,42 @@ const startPlugin = () => {
             if (event.type === "LOAD_MESSAGES_SUCCESS" && Array.isArray(event.messages)) {
                 event.messages = event.messages.filter((msg: any) => !filterMessage(msg));
             }
-
             if (event.type === "MESSAGE_CREATE" || event.type === "MESSAGE_UPDATE") {
                 if (filterMessage(event.message)) {
-					logger.log(`[ DiscordHideBlockUsers ]: event ${event.message}`);
                     event.cancel = true;
+                    logger.log(`[DiscordHideBlockUsers]: Filtered message from ${event.message.author?.username}`);
                 }
             }
         });
         patches.push(patch1);
-        
+
         const patch2 = before("generate", RowManager.prototype, ([data]: any) => {
             if (filterMessage(data.message)) {
-				logger.log(`[ DiscordHideBlockUsers ]: rowManager ${data.message}`);
                 data.cancel = true;
-				
+                logger.log(`[DiscordHideBlockUsers]: Filtered row for message from ${data.message.author?.username}`);
             }
         });
         patches.push(patch2);
 
-        logger.log(`[ DiscordHideBlockUsers ]: Plugin loaded`);
+        logger.log(`[DiscordHideBlockUsers]: Plugin loaded`);
     } catch (err) {
-        logger.error(`[ DiscordHideBlockUsers ]: Error:`, err);
+        logger.error(`[DiscordHideBlockUsers]: Error loading plugin`, err);
     }
 };
 
 export default {
     onLoad: () => {
-        logger.log(`[ DiscordHideBlockUsers ]: Plugin loading`);
-
+        logger.log(`[DiscordHideBlockUsers]: Plugin loading`);
         storage.blocked ??= true;
         storage.ignored ??= true;
-
         startPlugin();
     },
 
     onUnload: () => {
-        logger.log(`[ DiscordHideBlockUsers ]: Plugin unloading`);
-        for (const unpatch of patches) unpatch();
+        logger.log(`[DiscordHideBlockUsers]: Plugin unloading`);
+        patches.forEach(unpatch => unpatch());
         patches = [];
-        logger.log(`[ DiscordHideBlockUsers ]: Plugin unloaded`);
+        logger.log(`[DiscordHideBlockUsers]: Plugin unloaded`);
     },
 
     settings: Settings,

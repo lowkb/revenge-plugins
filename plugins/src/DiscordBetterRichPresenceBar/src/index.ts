9d33ce85645656bea9abc90ac5f4d02f45d7f1a5
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-28 20:56:16 +0100

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -1,69 +1,63 @@
 import { patcher, logger } from "@vendetta";
-import { findByTypeName, findByStoreName } from "@vendetta/metro";
+import { findByStoreName } from "@vendetta/metro";
 import { React } from "@vendetta/metro/common";
 import UserRichPresence from "./UserRichPresence";
 
 let unpatches: (() => void)[] = [];
 const patchedMap = new WeakMap<any, boolean>();
 
-async function waitForType(name: string) {
-    let comp;
-    while (!comp) {
-        comp = findByTypeName(name);
-        if (!comp) await new Promise(r => setTimeout(r, 50));
-    }
-    return comp;
-}
-
-function patchProfile(UserProfileContent: any, me: any) {
-    return patcher.after("type", UserProfileContent, (args, res) => {
-        const user = args[0]?.user;
-        if (!user) return res;
-
-        if (user.id !== me.id) return res;
-
-        const children = Array.isArray(res.props.children)
-            ? res.props.children
-            : [res.props.children];
-
-        const primaryInfo = children.find(
-            (c: any) => c?.type?.name === "PrimaryInfo"
-        );
-        if (!primaryInfo) return res;
-
-        if (!patchedMap.has(primaryInfo)) {
-            patchedMap.set(primaryInfo, true);
-
-            const existingChildren = Array.isArray(primaryInfo.props.children)
-                ? primaryInfo.props.children
-                : [primaryInfo.props.children];
-
-            primaryInfo.props.children = [
-                ...existingChildren,
-                React.createElement(UserRichPresence, {
-                    onRPC1: () => logger.log("[RPC] RPC 1 clicked"),
-                    onRPC2: () => logger.log("[RPC] RPC 2 clicked"),
-                })
-            ];
-
-            logger.log("[RPC] Added RPC buttons to profile for user id:", user.id);
-        }
-
-        return res;
-    });
-}
-
 export default {
-    onLoad: async () => {
+    onLoad: () => {
         logger.log("[RPC] Plugin loading...");
 
-        const UserProfileContent = await waitForType("UserProfileContent");
         const UserStore = findByStoreName("UserStore");
-        const me = UserStore.getCurrentUser();
+        const UserProfileStore = findByStoreName("UserProfileStore");
+        if (!UserStore || !UserProfileStore) {
+            logger.warn("[RPC] Stores not found!");
+            return;
+        }
 
+        const me = UserStore.getCurrentUser();
         logger.log("[RPC] Current user id:", me.id);
 
-        unpatches.push(patchProfile(UserProfileContent, me));
+        // patch renderowania profilu
+        unpatches.push(
+            patcher.after("getUserProfile", UserProfileStore, (args, res) => {
+                if (!res) return res;
+
+                const user = args[0];
+                if (!user || user.id !== me.id) return res;
+
+                const children = Array.isArray(res.props.children)
+                    ? res.props.children
+                    : [res.props.children];
+
+                const primaryInfo = children.find(
+                    (c: any) => c?.type?.name === "PrimaryInfo"
+                );
+                if (!primaryInfo) return res;
+
+                if (!patchedMap.has(primaryInfo)) {
+                    patchedMap.set(primaryInfo, true);
+
+                    const existingChildren = Array.isArray(primaryInfo.props.children)
+                        ? primaryInfo.props.children
+                        : [primaryInfo.props.children];
+
+                    primaryInfo.props.children = [
+                        ...existingChildren,
+                        React.createElement(UserRichPresence, {
+                            onRPC1: () => logger.log("[RPC] RPC 1 clicked"),
+                            onRPC2: () => logger.log("[RPC] RPC 2 clicked"),
+                        })
+                    ];
+
+                    logger.log("[RPC] Added RPC buttons to profile for user id:", user.id);
+                }
+
+                return res;
+            })
+        );
 
         logger.log("[RPC] Plugin loaded successfully");
     },

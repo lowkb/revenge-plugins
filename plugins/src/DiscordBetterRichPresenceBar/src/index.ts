6e3b7036d00a447d0cd63bda2b1cc6e007b4cd5a
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-28 21:35:35 +0100

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -1,65 +1,21 @@
-import { patcher } from "@vendetta";
-import { findByTypeName } from "@vendetta/metro";
-import { findInReactTree } from "@vendetta/utils";
-//no works
+import { logger } from "@vendetta";
+import { patchUserProfileContent } from "./patch/UserProfileContent";
+
 let unpatches: (() => void)[] = [];
 
 export default {
     onLoad: () => {
-        console.log("[RPC LOG] Plugin loading...");
+        logger.log("[RPC] Plugin loading...");
 
-        const UserProfileContent = findByTypeName("UserProfileContent");
-        if (!UserProfileContent) {
-            console.warn("[RPC LOG] UserProfileContent not found!");
-            return;
-        }
+        unpatches.push(...patchUserProfileContent());
 
-        unpatches.push(
-            patcher.after("type", UserProfileContent, (args, res) => {
-                console.log("[RPC LOG] UserProfileContent rendered with args:", args);
-                console.log("[RPC LOG] UserProfileContent render result:", res);
-
-                let primaryInfo = findInReactTree(res, (c) => c?.type?.name === "PrimaryInfo");
-                console.log("[RPC LOG] PrimaryInfo found:", primaryInfo);
-
-                if (!primaryInfo) return res;
-
-                patcher.after("type", primaryInfo, (args, res) => {
-                    console.log("[RPC LOG] PrimaryInfo render args:", args);
-                    console.log("[RPC LOG] PrimaryInfo render result:", res);
-
-                    if (res?.type?.name === "UserProfilePrimaryInfo") {
-                        patcher.after("type", res, (args, res) => {
-                            console.log("[RPC LOG] UserProfilePrimaryInfo render args:", args);
-                            console.log("[RPC LOG] UserProfilePrimaryInfo render result:", res);
-
-                            let displayName = findInReactTree(res, (c) => c?.type?.name === "DisplayName");
-                            console.log("[RPC LOG] DisplayName found:", displayName);
-
-                            if (!displayName) return res;
-
-                            patcher.after("type", displayName, (args, res) => {
-                                console.log("[RPC LOG] DisplayName render args:", args);
-                                console.log("[RPC LOG] DisplayName render result:", res);
-
-                                let userId = args[0]?.user?.id;
-                                console.log("[RPC LOG] Extracted userId:", userId);
-                            });
-                        });
-                    }
-                });
-
-                return res;
-            })
-        );
-
-        console.log("[RPC LOG] Plugin loaded, patchers installed for logging only");
+        logger.log("[RPC] Plugin loaded successfully");
     },
 
     onUnload: () => {
-        console.log("[RPC LOG] Unloading plugin...");
-        unpatches.forEach((u) => u());
+        logger.log("[RPC] Unloading plugin...");
+        unpatches.forEach(u => u());
         unpatches = [];
-        console.log("[RPC LOG] Plugin unloaded");
-    },
+        logger.log("[RPC] Plugin unloaded");
+    }
 };

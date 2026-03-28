0fa4e347411c02a8bb045bcc42bddae28a83923c
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-29 00:20:11 +0100

    Update ViewComponent.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
@@ -6,24 +6,10 @@ export default () => {
     const UserProfileCard = findByName("UserProfileCard");
     if (!UserProfileCard) return;
 
-    return before("render", UserProfileCard, (args, res) => {
-        const rpcWrapper = res?.props?.children?.[1];
-        if (!rpcWrapper) return res;
-
-        const children = rpcWrapper.props?.children;
-
-        if (Array.isArray(children)) {
-            logger.log("RPC children count:", children.length);
-
-            children.forEach((child, i) => {
-                logger.log(`Child[${i}] type:`, child?.type?.name ?? child?.type);
-                logger.log(`Child[${i}] props keys:`, Object.keys(child?.props ?? {}));
-            });
-        } else {
-            logger.log("RPC single child type:", children?.type?.name ?? children?.type);
-            logger.log("RPC single child props keys:", Object.keys(children?.props ?? {}));
-        }
-
-        return res;
+    return before("render", UserProfileCard, (args) => {
+            const userProps = wrapper.children?.[1]?.props;
+            const presenceProps = wrapper.children?.[3]?.props;
+        
+        logger.log(userProps.user?.id, presenceProps)
     });
 };

9b91a5e1ca531461a08e952fbd3234e79182237f
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-28 22:09:48 +0100

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -14,14 +14,8 @@ const patcher = () => {
 
 export default {
     onLoad: async () => {
-        try {
             unpatch = patcher();
             logger.info("Plugin loaded successfully.");
-        } catch(err) {
-            logger.info("Plugin crash on load\n\n", err);
-            showToast("Plugin crashed on load. Check debug log.");
-            stopPlugin("pluginId"); // zamień "pluginId" na id pluginu
-        }
     },
     onUnload: () => {
         unpatch?.();

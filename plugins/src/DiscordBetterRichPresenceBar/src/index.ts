8c22c11df4c2ca99c67f68f2944b335ba7f060e7
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-28 21:58:02 +0100

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -1,28 +1,30 @@
-import { logger } from "@vendetta";
+import { logger, showToast, stopPlugin } from "@vendetta";
 import ViewComponent from "./patches/ViewComponent";
 
 let unpatch = null;
+
 const patches = [
-        [ViewComponent, []]
+    [ViewComponent, []]
 ];
-const patcher = () => patches.forEach(([fn, args]) => fn(...args));
+
+const patcher = () => {
+    const unpatches = patches.map(([fn, args]) => fn(...args));
+    return () => unpatches.forEach(u => u?.());
+};
 
 export default {
-        onLoad: async () => {
-
-                
-                try {
-                        unpatch = patcher();
-                }
-                catch(err) {
-                        logger.info(`${pluginNameToast} Crash On Load.\n\n`, err)
-                        showToast(`${pluginNameToast} Crashing On Load. Please check debug log for more info.`)
-                        stopPlugin(id)                
-                }
-        },
-        onUnload: () => {
-
-        unpatch?.()
-        // stopPlugin(id)
+    onLoad: async () => {
+        try {
+            unpatch = patcher();
+            logger.info("Plugin loaded successfully.");
+        } catch(err) {
+            logger.info("Plugin crash on load\n\n", err);
+            showToast("Plugin crashed on load. Check debug log.");
+            stopPlugin("pluginId"); // zamień "pluginId" na id pluginu
+        }
+    },
+    onUnload: () => {
+        unpatch?.();
+        logger.info("Plugin unloaded.");
     }
-}
+};

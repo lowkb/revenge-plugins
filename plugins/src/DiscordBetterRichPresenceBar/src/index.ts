9f38fdc02ab8bfaf9657f52d06f7a75d9f52926f
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-28 19:57:21 +0100

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -1,17 +1,15 @@
 import { patcher } from "@vendetta";
 import { findByTypeName, findByStoreName } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
-import React from "react";
-import { General } from "@vendetta/ui/components";
+import UserRichPresence from "./UserRichPresence";
 import Settings from "./settings";
-const { View, Button } = General;
+import React from "react";
 
 let unpatches: (() => void)[] = [];
 
 export default {
     onLoad: () => {
         storage.showRPCButtons ??= true;
-
         if (!storage.showRPCButtons) return;
 
         const UserProfileContent = findByTypeName("UserProfileContent");
@@ -31,23 +29,10 @@ export default {
                 primaryInfo._rpcButtonsPatched = true;
 
                 primaryInfo.props.children.push(
-                    <View
-                        key="RPCButtons"
-                        style={{
-                            flexDirection: "row",
-                            justifyContent: "flex-start",
-                            marginTop: 8
-                        }}
-                    >
-                        <Button
-                            label="RPC 1"
-                            onPress={() => createRPC("action1")}
-                        />
-                        <Button
-                            label="RPC 2"
-                            onPress={() => createRPC("action2")}
-                        />
-                    </View>
+                    React.createElement(UserRichPresence, {
+                        onRPC1: () => console.log("RPC 1 triggered"),
+                        onRPC2: () => console.log("RPC 2 triggered")
+                    })
                 );
 
                 return res;

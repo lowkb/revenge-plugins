851b70fc7e75db7a6f7457df7a3bdd07cfd18cde
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-30 15:55:52 +0200

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -1,25 +1,16 @@
-import { logger, showToast, stopPlugin } from "@vendetta";
+import { logger } from "@vendetta";
 import ViewComponent from "./patches/ViewComponent";
 
-let unpatch = null;
-
-const patches = [
-[ViewComponent, []]
-];
-
-const patcher = () => {
-const unpatches = patches.map(([fn, args]) => fn(...args));
-return () => unpatches.forEach(u => u?.());
-};
+let unpatch: (() => void) | null = null;
 
 export default {
-onLoad: async () => {
-unpatch = patcher();
-logger.info("Plugin loaded successfully.");
-},
-onUnload: () => {
-unpatch?.();
-logger.info("Plugin unloaded.");
-}
+    onLoad() {
+     //   unpatch = ViewComponent();
+        logger.info("Plugin loaded successfully.");
+    },
+    onUnload() {
+        unpatch?.();
+        unpatch = null;
+        logger.info("Plugin unloaded.");
+    }
 };
-

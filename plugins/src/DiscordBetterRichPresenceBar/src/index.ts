680307b24632d25bb376e2415ddd390d843237af
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-30 21:05:52 +0200

    Update index.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/index.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/index.ts
@@ -1,36 +1,29 @@
 import { patcher } from "@vendetta";
-import { findByDisplayName, findByName, findByProps, findByPropsAll, findByStoreName, findByTypeNameAll, findByTypeName } from "@vendetta/metro";
-import { General } from "@vendetta/ui/components";
-import { findInReactTree } from "@vendetta/utils";
-import StatusIcons from "./StatusIcons";
-import { storage } from "@vendetta/plugin";
-import PresenceUpdatedContainer from "./PresenceUpdatedContainer";
-import React from "react";
-
-const { Text, View } = General;
+import { findByTypeName, findInReactTree } from "@vendetta/metro";
 
 let unpatches: Function[] = [];
 
 export default {
     onLoad: () => {
-        storage.dmTopBar ??= true;
-        storage.userList ??= true;
-        storage.profileUsername ??= true;
-        storage.removeDefaultMobile ??= true;
-        storage.oldUserListIcons ??= false;
-
-        const debugLabels = false;
-
-        // --- PATCH: Profile Content ---
         const UserProfileContent = findByTypeName("UserProfileContent");
-        if (UserProfileContent) {
-            unpatches.push(patcher.after("type", UserProfileContent, (_, res) => {
-                const primaryInfo = findInReactTree(res, c => c?.type?.name === "PrimaryInfo");
-                if (!primaryInfo) return res;
+        if (!UserProfileContent) return;
 
-                patcher.after("type", primaryInfo, (_, primaryRes) => {
-                    const displayName = findInReactTree(primaryRes, c => c?.type?.name === "DisplayName");
-                    if (!displayName) return primaryRes;
+        unpatches.push(
+            patcher.after("type", UserProfileContent, (_, res) => {
+                // Szukamy komponentu Activity w drzewie Reacta
+                const activity = findInReactTree(res, c => c?.type?.name === "Activity");
+                if (activity) {
+                    console.log("Activity znalezione:", activity);
+                } else {
+                    console.log("Activity NIE znalezione w tym profilu");
+                }
+                return res;
+            })
+        );
+    },
 
-                    patcher.after("type", displayName, (args, displayRes) => {
-                        const userId = args[0]?.user
+    onUnload: () => {
+        unpatches.forEach(u => u());
+        unpatches = [];
+    }
+};

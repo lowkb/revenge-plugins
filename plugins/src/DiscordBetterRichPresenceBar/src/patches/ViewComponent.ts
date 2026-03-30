3402618c8d1762eace1bd2de40e020c974f3eeea
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-30 11:55:27 +0200

    Update ViewComponent.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
@@ -3,34 +3,16 @@ import { findByName } from "@vendetta/metro";
 import { logger } from "@vendetta";
 
 export default () => {
-    logger.info("[DiscordBetterRichPresenceBar] ViewComponent patch init");
+    logger.info("[DiscordBetterRichPresenceBar] init");
 
     const UserProfileCard = findByName("UserProfileCard");
 
     if (!UserProfileCard) {
-        logger.error("[DiscordBetterRichPresenceBar] UserProfileCard not found");
+        logger.error("[DiscordBetterRichPresenceBar] not found");
         return;
     }
 
-    logger.info("[DiscordBetterRichPresenceBar] UserProfileCard found");
-
     return after(UserProfileCard, (args, res) => {
-        try {
-            logger.info("[DiscordBetterRichPresenceBar] component called");
-
-            const props = args?.[0];
-            const wrapper = res;
-
-            const userId = props?.user?.id;
-
-            logger.info("[DiscordBetterRichPresenceBar] userId:", userId);
-
-            const userProps = wrapper?.children?.[1]?.props;
-            const presenceProps = wrapper?.children?.[3]?.props;
-
-            logger.info("[DiscordBetterRichPresenceBar] presence:", presenceProps);
-        } catch (err) {
-            logger.error("[DiscordBetterRichPresenceBar] patch error", err);
-        }
+        logger.info("[DiscordBetterRichPresenceBar] called");
     });
 };

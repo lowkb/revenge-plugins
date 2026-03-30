bc7e0649dc34809d15113279badf5a229c7639cb
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-30 16:49:20 +0200

    Update ViewComponent.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
@@ -4,25 +4,10 @@ import { logger } from "@vendetta";
 
 
 export default () => {
-    const UserProfile = findByName("UserProfile");
+    const UserProfile = findByName("UserProfileStore");
     
     if (!UserProfile) return;
-    logger.log(UserProfile.user.id)
+    logger.log(UserProfile.UserStore.getCurrentUser())
     return before("render", UserProfile, (args) => {
-
-        const wrapper = args[0];
-        if (!wrapper || !Array.isArray(wrapper.children)) return;
-
-        // Szukamy dziecka, które ma status / rich presence
-        const userInfo = wrapper.children.find(c => c?.props?.user);
-        const presence = wrapper.children.find(c => c?.props?.status);
-
-        if (!userInfo || typeof userInfo.props.user?.id !== "string") return;
-        if (!presence || typeof presence.props.status !== "string") return;
-
-        const userId = userInfo.props.user.id;
-        const status = presence.props.status;
-
-        logger.log(userId, status);
     });
 };

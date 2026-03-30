97882c4b4ee7d4e574534250c7158442bdadb581
low_kb <69186337+lowkb@users.noreply.github.com>
2026-03-30 11:59:29 +0200

    Update ViewComponent.ts

--- plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
+++ plugins/src/DiscordBetterRichPresenceBar/src/patches/ViewComponent.ts
@@ -1,18 +1,22 @@
-import { after } from "@vendetta/patcher";
-import { findByName } from "@vendetta/metro";
-import { logger } from "@vendetta";
+export default () => before("render", General.View, (args) => {
+        if(!isEnabled) return;
 
-export default () => {
-    logger.info("[DiscordBetterRichPresenceBar] init");
+    const [wrapper] = args;
+    if (!wrapper || !Array.isArray(wrapper.style)) return;
 
-    const UserProfileCard = findByName("UserProfileCard");
 
-    if (!UserProfileCard) {
-        logger.error("[DiscordBetterRichPresenceBar] not found");
-        return;
+            const userProps   = wrapper.children?.[1]?.props;
+                const presenceProps = wrapper.children?.[3]?.props;
+
+        
+                if (!userProps?.hasOwnProperty("user") || typeof userProps.user?.id !== "string") return;
+                if (!presenceProps?.hasOwnProperty("status") || typeof presenceProps.status !== "string") return;
+
+                console.log(userProps.user?.id, presenceProps)
+
+                const userPresence = presenceProps.status;
+
+                
+            }           
     }
-
-    return after(UserProfileCard, (args, res) => {
-        logger.info("[DiscordBetterRichPresenceBar] called");
-    });
-};
+});
